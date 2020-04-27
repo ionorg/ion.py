3c50773bedc00d2412cc98267706f864e3660030
Tarrence van As <tarrence13@gmail.com>
2020-04-27 11:31:06 -0400

    pip stuff

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='ion',
+    version='0.1',
+    description='Ion service discovery for python services',
+    long_description='Asyncio based Python serice disconvery client for ION.',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    url='https://github.com/pion/ion.py',
+    author="Tarrence van As",
+    author_email="tarrence13@gmail.com",
+    license='MIT License',
+    packages=['ion'],
+    zip_safe=True
+)
